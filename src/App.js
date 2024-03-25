58c4fdedbe645de8461850b5c2ef9a2e889fa438
marcinoles87 <marcinoles87@gmail.com>
2024-03-25 16:34:50 +0100

    work with code

--- src/App.js
+++ src/App.js
@@ -86,6 +86,8 @@ function App() {
       <button onClick={handleAdd}>Dodaj nowe Dane do faktury</button>
       <h1>Dane pobrane z bazy danych  MYSQL </h1>
       <button onClick={handleGetData}>Show data BASE</button>
+    
+    
      {data.map( (item , index) =>{
       
       return(
@@ -93,7 +95,7 @@ function App() {
         <div key={index} className='item-container'>
           <>
 
-          <div className='faktura-dane'>
+          <div className='faktura_dane'>
             <p>Faktura numer : 6/2024</p>
             <p>Data wystawienia : 25.03.2024 </p>
             <p>Data sprzedaży : 26.03.2024</p>
@@ -101,26 +103,65 @@ function App() {
 
           </div>
 
-        <div className='item-sell'>
+        <div className='item_sell'>
       <h3>Sprzedawca:</h3>
             <p className='item-name'>Nazwa : Grażyna Oleś Usługi porządkowe</p>
             <p  className='item-adres'>Adres : Osiedle na Wzgórzach 15/7</p>
             <p  className='item-nip'>Nip : 677-100-283</p>
-            <p  className='item-date'>Data : 22-03-2024</p>
             <p className='item-desc'>Opis : Usługa porządkowa</p>
             <button onClick={() => handleDelete(item)}>Usun</button>
            
         </div>
 
 
-        <div className='item-sell'>
+        <div className='item_sell'>
            <h3>Nabywca:</h3>
             <p className='item-name'>Nazwa :{item.Nazwa}</p>
             <p  className='item-adres'>Adres : {item.Adres}</p>
             <p  className='item-nip'>Nip :{item.Nip}</p>
-            <p  className='item-date'>Data :{item.Data}</p>
+            {/* <p  className='item-date'>Data :{item.Data}</p> */}
             <p className='item-desc'>Opis :{item.Opis}</p>
             <button onClick={() => handleDelete(item)}>Usun</button>
+        </div>
+
+        <div className='faktura_pozycja'>
+        <table>
+              <tr>
+                <td>Lp.</td>
+                <td>Nazwa towaru / usługi</td>
+                <td>Ilośc</td>
+                <td>Cena netto</td>
+                <td>Cena brutto</td>
+                <td>Wartość netto</td>
+                <td>VAT %</td>
+                <td>Wartość VAT</td>
+                <td>Wartość brutto</td>
+              </tr>
+
+                <tr>
+                <td>1</td>
+                <td>Dezynfekcja i sprzątanie pomieszczeń po wszawicy</td>
+                <td>1</td>
+                <td>1000</td>
+                <td>1230</td>
+                <td>1000</td>
+                <td>23</td>
+                <td>230</td>
+                <td>1230</td>
+                </tr>
+</table>
+        </div>
+
+        <div className='faktura_podsumowanie'>
+          <p> <b>Wartość netto :</b>  1000 PLN</p>
+          <p> <b>Wartość VAT :</b>  230 PLN</p>
+          <p> <b>Wartość brutto :</b>  1230 PLN</p>
+         
+        </div>
+
+        <div className='faktura_do_zaplaty'>
+          <h3>Do zapłaty </h3>
+          <p> 1230 PLN tysiąc dwieście trzydzieści PLN </p>
         </div>
          
            
