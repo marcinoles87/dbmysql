82ccd96cf1e1c23e3009618584ba0289a9420c54
marcinoles87 <marcinoles87@gmail.com>
2024-07-30 15:13:43 +0200

    add section

--- src/backend/server.js
+++ src/backend/server.js
@@ -26,7 +26,7 @@ app.post("/" , (req , res) => {
     const Nip = req.body.Nip;
     const Cena = req.body.Cena;
 
-    db.query("INSERT INTO wydarzenia (Nazwa , Data , Opis , Adres , Nip ) VALUES ( ? , ? , ? , ? , ? , ?  )" , [Nazwa,Data,Opis,Adres,Nip,Cena] , 
+    db.query("INSERT INTO wydarzenia (Nazwa , Data , Opis , Adres , Nip , Cena ) VALUES ( ? , ? , ? , ? , ? , ?  )" , [Nazwa,Data,Opis,Adres,Nip,Cena] , 
         (err , result) => {
             console.log(result)
             if(err) {
